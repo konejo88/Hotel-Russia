daffc827eaa00bcd0958150b725f8d17c65b3519
konejo88 <konejo@mail.ru>
2023-04-13 21:16:29 +0300

    Hotel Russia

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("gosti")
+
+document.querySelector(".stefania").onclick=function(){
+	this.classList.toggle("stefania1")
+}
+	document.querySelector(".stefania2").onclick=function(){
+	this.classList.toggle("stefania3")
+}
+document.querySelector(".desert1").onclick=function(){
+	this.classList.toggle("desert2")
+}
+document.querySelector(".desert3").onclick=function(){
+	this.classList.toggle("desert4")
+}
+// menu
+const menus=document.createElement("div");
+div.classList.add("menu")
+const menu=document.querySelector(".menu")
